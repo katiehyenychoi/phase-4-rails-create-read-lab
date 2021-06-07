0c660de1866975a01a5a4d98204bcba25e6c3d0c
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-06-07 16:04:24 -0400

    commit tree 607f94c289f5ec221e2690a2d0d6ceceabd08276

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,27 @@
+class PlantsController < ApplicationController
+
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plants = Plant.find_by(id: params[:id])
+    if plants
+      render json: plants
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  private
+
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
+end
